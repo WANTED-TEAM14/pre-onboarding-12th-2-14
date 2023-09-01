3077efbe352cf7c378ce822da089473e57cdd715
nearworld <artemismars2@outlook.com>
2023-09-01 09:08:05 +0900

    feat: Octokit 에러에 맞게 handleError 구현

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,11 +4,16 @@ import { getIssues } from 'apis';
 import { useLocation } from 'react-router-dom';
 import { IssueType } from 'types';
 
+type OctokitError = {
+  status: 403 | 404 | 422;
+};
+
 function useFetch({ currentNum }: { currentNum: number }) {
   const { pathname } = useLocation();
   const [issueList, setIssueList] = useState<IssueType[]>([]);
   const [isShowError, setIsShowError] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const fetchIssues = async () => {
@@ -20,6 +25,7 @@ function useFetch({ currentNum }: { currentNum: number }) {
         });
       } catch (e) {
         setIsShowError(true);
+        handleError(e);
       } finally {
         setLoading(false);
       }
@@ -27,7 +33,20 @@ function useFetch({ currentNum }: { currentNum: number }) {
     fetchIssues();
   }, [currentNum, pathname]);
 
-  return { issueList, loading, isShowError };
+  const handleError = (error: unknown) => {
+    if (error && typeof error === 'object' && 'status' in error) {
+      const { status } = error as OctokitError;
+      setErrorMessage(
+        {
+          404: '찾고자하는 데이터가 없습니다.',
+          403: 'API 요청 제한 횟수 초과로 인해 요청이 제한되었습니다.',
+          422: '인증이 실패했거나 너무 많은 요청 시도가 있습니다.',
+        }[status],
+      );
+    } else setErrorMessage('Unknown Network Error');
+  };
+
+  return { issueList, loading, isShowError, errorMessage };
 }
 
 export default useFetch;
